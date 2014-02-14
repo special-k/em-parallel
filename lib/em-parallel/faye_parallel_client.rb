67ee67c9f02153750c5d0fe9e04941af0d82256a
Kirill Jakovlev <special-k@li.ru>
2014-02-14 09:01:53 +0600

    ws accessor

--- lib/em-parallel/faye_parallel_client.rb
+++ lib/em-parallel/faye_parallel_client.rb
@@ -5,22 +5,22 @@ require 'msgpack'
 
 class FayeParallelClient
   PACK_KEY = 'c*'
-  attr_reader :ws
+  attr_reader :ws, :url
   def initialize url, use_fiber_on_start = false
     @fibers = {}
-    @ws = Faye::WebSocket::Client.new url
-
+    @url = url
+    @ws = Faye::WebSocket::Client.new "ws://#{ url }"
 
     @ws.on :message do |event|
-      #data = Yajl::Parser.parse( event.data )
       data = Marshal.load event.data.pack PACK_KEY
-      #data = MessagePack.unpack event.data.pack PACK_KEY
       k = data[0]
       if  @fibers.include? k
         @fibers.delete(k).resume data[1]
       end
     end
 
+    #to send the message immediately after a connection (for example)
+    #(initialization must occur within a fiber)
     if use_fiber_on_start
       @ws.on :open do |event|
         @open_fiber.resume
@@ -35,9 +35,20 @@ class FayeParallelClient
     k = SecureRandom.base64
     fiber = Fiber.current
     @fibers[k] = fiber
-    #@ws.send Yajl::Encoder.encode([k,v])
     @ws.send Marshal.dump([k,v]).bytes
-    #@ws.send MessagePack.dump([k,v]).bytes
     Fiber.yield
   end
+
+  def simple_send v
+    @ws.send Marshal.dump(v).bytes
+  end
+
+  def to_hash
+    { url: @url }
+  end
+
+  def close
+    @ws.close
+  end
+
 end
